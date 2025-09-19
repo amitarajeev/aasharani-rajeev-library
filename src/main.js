6e84beebbf24562c32fa517a8da04f6459d72a2f
amitarajeev <ammuasha12@gmail.com>
2025-09-19 19:38:44 +1000

    Task 7.1 completed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // import './assets/main.css'
 // import '@/assets/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { initializeApp } from "firebase/app";
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -18,3 +19,15 @@ app.use(router)
 // app.component('Column', Column)
 
 app.mount('#app')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYsBs3Ylspn6rcAuG9apccnbuMXd1nffg",
+  authDomain: "week7-amita.firebaseapp.com",
+  projectId: "week7-amita",
+  storageBucket: "week7-amita.firebasestorage.app",
+  messagingSenderId: "957263373287",
+  appId: "1:957263373287:web:88b76002da474a5a981342"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
